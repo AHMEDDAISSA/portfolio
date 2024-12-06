72979dd9afe3931444d2650f6b4a7e63d8ccf66f
Ahmed <ahmedaissa575@gmail.com>
2024-12-06 19:39:50 +0100

    refactor : navigation 'home'

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 
 const drawerWidth = 240;
-const navItems = [ ['About', 'about-me'],['Experience', 'expertise'], ['Projects', 'projects'], ['Resume', 'contact']];
+const navItems = [ ['Home', 'home'],['About', 'about-me'],['Experience', 'expertise'], ['Projects', 'projects'], ['Resume', 'contact']];
 
 function Navigation({parentToChild, modeChange}: any) {
 
